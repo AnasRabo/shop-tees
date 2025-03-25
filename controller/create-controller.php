6dfd7cede21e5656f04d2c3e689ef5fd7111a64b
AnasRabo <anas.rabo@lapiscine.pro>
2025-03-25 16:18:11 +0100

    ajout de commentaire

--- controller/create-controller.php
+++ controller/create-controller.php
@@ -2,15 +2,15 @@
 require_once('../config.php');
 
 
-    $message = "";
+    $message = "";  /// on crée une variable vide.
 
-    if(array_key_exists("quantity" , $_POST)&&
-       array_key_exists("product", $_POST)){
+    if(array_key_exists("quantity" , $_POST)&&  /// si "quantity" qui est dans POST contient une valeur.
+       array_key_exists("product", $_POST)){    /// si "product" qui est dans POST contient une valeur.
 
-        $product = $_POST["product"];
-        $quantity = $_POST["quantity"];
+        $product = $_POST["product"];   ///on crée une variable $product egale a  $_POST["product"].
+        $quantity = $_POST["quantity"]; ///on crée une variable $product egale a  $_POST["quantity"].
 
-        $message = "votre commande contient :   " . $quantity . " " . $product;
+        $message = "votre commande contient :   " . $quantity . " " . $product; /// on rapelle la variable $message et on lui donne un valeur qui contient des chaine de caractères et les deux variables.
        }
 
-       require_once('../view/create-view.php');
+       require_once('../view/create-view.php');git add <div class="git cp"></div>
--- view/create-view.php
+++ view/create-view.php
@@ -8,14 +8,14 @@
 <body>
 
 <h3><?php echo $message?></h3>
-    <form method="POST">
-        <select name="product">
-            <option value="">maillot football</option>
-            <option value="second" selected>maillot rugby</option>
-            <option value="third">maillot hand-ball</option>
+    <form method="POST"> <!-- on crée un forme avec une methode select pour pouvoir recupérer les infos du client . -->
+        <select name="product"><!-- création d'un select qui contient 3 option .-->
+            <option value="maillot-football">maillot football</option>
+            <option value="maillot-rugby" selected>maillot rugby</option>
+            <option value="maillot-hand-ball">maillot hand-ball</option>
         </select>
-        <label><input name="quantity" type="number">quantity</label>
-        <button type = "submit">envoyer !</button>
+        <label><input name="quantity" type="number">quantity</label><!-- création d'un label de type number . -->
+        <button type = "submit">envoyer !</button><!-- création d'un button de type "submit" . -->
            
     </form>
 </body>
