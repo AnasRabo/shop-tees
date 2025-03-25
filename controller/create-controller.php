15b9579fb6d2b7370eac6a3c660d348d6458a826
AnasRabo <anas.rabo@lapiscine.pro>
2025-03-25 16:01:43 +0100

    Création et verification du Form

--- controller/create-controller.php
+++ controller/create-controller.php
@@ -1,2 +1,16 @@
 <?php
-require_once('../view/create-view.php');
+require_once('../config.php');
+
+
+    $message = "";
+
+    if(array_key_exists("quantity" , $_POST)&&
+       array_key_exists("product", $_POST)){
+
+        $product = $_POST["product"];
+        $quantity = $_POST["quantity"];
+
+        $message = "votre commande contient :   " . $quantity . " " . $product;
+       }
+
+       require_once('../view/create-view.php');
--- view/create-view.php
+++ view/create-view.php
@@ -6,9 +6,17 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="">
-        <label><input type="text">quantity</label>
+
+<h3><?php echo $message?></h3>
+    <form method="POST">
+        <select name="product">
+            <option value="">maillot football</option>
+            <option value="second" selected>maillot rugby</option>
+            <option value="third">maillot hand-ball</option>
+        </select>
+        <label><input name="quantity" type="number">quantity</label>
         <button type = "submit">envoyer !</button>
+           
     </form>
 </body>
 </html>
